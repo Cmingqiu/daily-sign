ef97195301c1d69ce678d8065d0fdbdb97d017f9
chenmq <1075124372@qq.com>
2024-03-29 09:55:25 +0800

    feat: 集成uview-plus;增加类型提示

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,23 @@
 /// <reference types="vite/client" />
 
 declare module '*.vue' {
-  import { DefineComponent } from 'vue'
+  import { DefineComponent } from 'vue';
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
-  const component: DefineComponent<{}, {}, any>
-  export default component
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
+
+declare module 'uview-plus' {
+  import { App } from 'vue';
+  const Component = (app: App) => unknown;
+  global {
+    interface Uni {
+      $u: {
+        config: {
+          unit: string;
+        };
+      };
+    }
+  }
+  export default Component;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
-import { createSSRApp } from "vue";
-import App from "./App.vue";
+import { createSSRApp } from 'vue';
+import App from './App.vue';
+
+import uviewPlus from 'uview-plus';
+
 export function createApp() {
   const app = createSSRApp(App);
+  app.use(uviewPlus);
+  // uview-plus设置默认单位为rpx
+  uni.$u.config.unit = 'rpx';
+
   return {
-    app,
+    app
   };
 }
