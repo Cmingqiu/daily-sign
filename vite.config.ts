af4ff1d08dc27aae44406f0aac39d34d2645c522
chenmq <1075124372@qq.com>
2024-04-22 12:18:37 +0800

    feat: 修改vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,18 @@ import { defineConfig } from 'vite';
 import uni from '@dcloudio/vite-plugin-uni';
 import UnoCSS from 'unocss/vite';
 
+const buildOptions = {
+  watch: {
+    exclude: ['node_modules/**', '/__uno.css']
+  }
+};
+const isDev = process.env.NODE_ENV === 'development';
+
 export default defineConfig({
   build: {
     sourcemap: process.env.NODE_ENV === 'development',
-    watch: {
-      exclude: ['node_modules/**', '/__uno.css']
-    }
+    // 开发时bug: resource busy or locked, lstat 'C:\DumpStack.log.tmp'
+    ...(isDev && buildOptions)
   },
   plugins: [uni(), UnoCSS()]
 });
