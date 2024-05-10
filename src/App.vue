37cae8c86148ce6879ddf4d55056a997f3e092f6
chenmq <1075124372@qq.com>
2024-05-10 15:36:41 +0800

    feat: 测试获取微信头像昵称

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,6 @@
 import { onLaunch, onShow, onHide } from '@dcloudio/uni-app';
 onLaunch(params => {
   console.log('App Launch', params?.scene);
-  wx.getUserInfo({
-    desc: '获取用户信息',
-    success(res) {
-      console.log('获取用户信息', res);
-    },
-    fail(res) {
-      console.log('fail', res);
-    }
-  });
 });
 onShow(() => {
   console.log('App Show');
--- src/pages/mine/index.vue
+++ src/pages/mine/index.vue
@@ -1,14 +1,49 @@
 <template>
   <LayoutContainer>
     <view flex items-center>
-      <view class="rounded-[50%]" w-100 h-100 mr-3 overflow-hidden>
-        <open-data type="userAvatarUrl" />
-      </view>
+      <button
+        class="rounded-[50%]"
+        w-100
+        h-100
+        ml-3
+        mr-3
+        px-0
+        overflow-hidden
+        text-center
+        open-type="chooseAvatar"
+        @chooseavatar="chooseAvatar">
+        <!-- <open-data type="userAvatarUrl" /> -->
+        <image w-100 h-100 :src="avatarUrl" mode="scaleToFill" />
+      </button>
+
       <open-data font-bold type="userNickName" />
     </view>
   </LayoutContainer>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+import { onLoad } from '@dcloudio/uni-app';
+
+const defaultAvatar =
+  'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0';
+const avatarUrl = ref(defaultAvatar);
+const chooseAvatar = (e: any) => {
+  avatarUrl.value = e.detail.avatarUrl;
+  /* uni.getUserProfile({
+    desc: 'xx',
+    success(result) {
+      console.log('getUserProfile ', result);
+    }
+  }); */
+};
+onLoad(() => {
+  uni.getSetting({
+    success(res) {
+      console.log('getSetting ', res);
+    }
+  });
+});
+</script>
 
 <style lang="scss" scoped></style>
